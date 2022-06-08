22ac0c1a7f4812ee617d9fbda7219366624e3819
asmindev <asmin987asmin@gmail.com>
2022-06-08 15:20:34 +0800

    fix tailwindcss rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,6 @@ module.exports = {
         montserrat: ['Montserrat', 'sans-serif'],
         outfit: ['Outfit', 'sans-serif']
       },
-      animation: {
-        blob: 'blob 7s infinite'
-      },
-      keyframes: {
-        blob: {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(30px, -50px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-20px, 20px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          }
-
-        }
-      }
     },
   },
   plugins: [],
