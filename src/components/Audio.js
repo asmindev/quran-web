67ba85d29382be121777912e53444f1f8fa40c19
asmindev <asmin987asmin@gmail.com>
2022-06-14 09:13:27 +0800

    Esktrak audio to component

---
+++ src/components/Audio.js
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from 'react'
+
+export default function Audio({ audio }) {
+  const [playing, setPlaying] = useState(false)
+  const [onPlay, setOnPlay] = useState(false)
+  const [duration, setDuration] = useState(0)
+  const audioRef = useRef(null)
+  const play = (e) => {
+    e.preventDefault()
+    if (!playing) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+    setPlaying(!playing)
+    setOnPlay(!onPlay)
+  }
+  return (
+    <div className="w-full">
+      <div className="w-full flex gap-2 items-center justify-between">
+        <button
+          className="max-w-min text-xl relative -top-3"
+          onClick={play}
+          type="button"
+        >
+          <p
+            className={`${
+                !onPlay
+                  ? 'delay-100 rotate-0 opacity-100'
+                  : 'rotate-90 opacity-0'
+              } transition-all duration-200 h-5 absolute top-0`}
+          >
+            <ion-icon name="play-outline" />
+          </p>
+          <p
+            className={`${
+                onPlay
+                  ? 'delay-100 rotate-0 opacity-100'
+                  : 'opacity-0 rotate-90'
+              } transition-all duration-200 h-5 absolute top-0`}
+          >
+            <ion-icon name="pause-outline" />
+          </p>
+        </button>
+        <div className="w-[90%] h-1 bg-indigo-100/80">
+          <div
+            role="progressbar"
+            className="h-1 bg-indigo-400 rounded-lg transition-all duration-300"
+            aria-valuenow={duration}
+            style={{ width: `${parseInt(duration, 10)}%` }}
+            aria-label="save"
+          />
+        </div>
+      </div>
+      <audio
+        onEnded={() => {
+            setOnPlay(!onPlay)
+            setPlaying(!playing)
+            setDuration(0)
+          }}
+        onLoad={console.log('loadded')}
+        ref={audioRef}
+        className="w-full"
+        onTimeUpdate={(e) => setDuration((e.target.currentTime / e.target.duration) * 100)}
+        src={audio.primary}
+      >
+        <track kind="captions" />
+      </audio>
+    </div>
+  )
+}
--- src/components/Ayat.js
+++ src/components/Ayat.js
@@ -1,20 +1,7 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
+import Audio from './Audio'
 
 export default function Surah({ surah }) {
-  const [playing, setPlaying] = useState(false)
-  const [onPlay, setOnPlay] = useState(false)
-  const [duration, setDuration] = useState(0)
-  const audioRef = useRef(null)
-  const play = (e) => {
-    e.preventDefault()
-    if (!playing) {
-      audioRef.current.play()
-    } else {
-      audioRef.current.pause()
-    }
-    setPlaying(!playing)
-    setOnPlay(!onPlay)
-  }
   return (
     <div className="w-full h-full p-4 flex flex-wrap flex-col gap-4 border border-indigo-100 rounded-lg">
       <div className="w-full flex">
@@ -35,56 +22,8 @@ export default function Surah({ surah }) {
           {surah.translation.id}
         </h2>
       </div>
-      <div className="w-full">
-        <div className="w-full flex gap-2 items-center justify-between">
-          <button
-            className="max-w-min text-xl relative -top-3"
-            onClick={play}
-            type="button"
-          >
-            <p
-              className={`${
-                !onPlay
-                  ? 'delay-100 rotate-0 opacity-100'
-                  : 'rotate-90 opacity-0'
-              } transition-all duration-200 h-5 absolute top-0`}
-            >
-              <ion-icon name="play-outline" />
-            </p>
-            <p
-              className={`${
-                onPlay
-                  ? 'delay-100 rotate-0 opacity-100'
-                  : 'opacity-0 rotate-90<F2>'
-              } transition-all duration-200 h-5 absolute top-0`}
-            >
-              <ion-icon name="pause-outline" />
-            </p>
-          </button>
-          <div className="w-[90%] h-1 bg-indigo-200">
-            <div
-              role="progressbar"
-              className="h-1 bg-indigo-400 rounded-lg transition-all duration-100"
-              aria-valuenow={duration}
-              style={{ width: `${parseInt(duration, 10)}%` }}
-              aria-label="save"
-            />
-          </div>
-        </div>
-        <audio
-          onEnded={() => {
-            setOnPlay(!onPlay)
-            setPlaying(!playing)
-            setDuration(0)
-          }}
-          onLoad={console.log('loadded')}
-          ref={audioRef}
-          className="w-full"
-          onTimeUpdate={(e) => setDuration((e.target.currentTime / e.target.duration) * 100)}
-          src={surah.audio.primary}
-        >
-          <track kind="captions" />
-        </audio>
+      <div className="w-full my-4">
+        <Audio audio={surah.audio} />
       </div>
     </div>
   )
